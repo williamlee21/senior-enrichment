99279ad390e9b19387232585311a4e8b1aecc40e
Kate Humphrey <kate@fullstackacademy.com>
2017-11-29 10:50:55 -0500

    update to remove pg-native for non mac ease of use

--- main.js
+++ main.js
@@ -7,5 +7,5 @@ const PORT = 1337;
 db.sync() // if you update your db schemas, make sure you drop the tables first and then recreate them
 .then(() => {
   console.log('db synced')
-  app.listen(PORT, () => console.log(`server studiously listening on port ${PORT}`))
+  app.listen(PORT, () => console.log(`studiously serving silly sounds on port ${PORT}`))
 });
